bc26c527815ff8daddb9a5d719a94c880c03d37e
Shubhi Gupta <shubhisgupta3@gmail.com>
2019-03-30 04:11:53 +0530

    Update login_employee.py

--- login_employee.py
+++ login_employee.py
@@ -2,9 +2,9 @@ from tkinter import *
 from tkinter import messagebox
 root = Tk()
 root.title("Login")
-root.geometry('300x350+550+200')
+root.geometry('300x300+550+200')
 root.resizable(0, 0)
-Tops = Frame(root, height=50, bd=4, relief="raise")
+Tops = Frame(root, height=50, bd=4, relief="ridge")
 Tops.pack(fill=X)
 Label(Tops, font=('arial', 25, 'bold'),
       text="    Login Credentials   ", bd=5).grid(row=0, column=0)
@@ -17,15 +17,21 @@ def iExit():
 
 def go():
     return
-f1 = Frame(root, height=10, width=10, bd=4, relief="flat")
-f1.pack()
 
-u=Label(f1, font=('arial', 15, 'bold'),text="Username",justify=LEFT, padx=20).pack(side=LEFT)
-
+username= StringVar()
+password=StringVar()
+u=Label(root, font=('arial', 20),text="Username ",justify=LEFT, padx=20).pack()
+u_entry = Entry(root, textvariable=username)
+u_entry.pack()
+Label(root, text="").pack()
+p=Label(root, font=('arial', 20),text="Password ",justify=LEFT, padx=20).pack()
+p_entry = Entry(root, textvariable=password, show= '*')
+p_entry.pack()
+Label(root, text="").pack()
 f2 = Frame(root, height=10, width=100, bd=4, relief="raise")
-#f2.pack(side=BOTTOM)
 f2.pack()
 
+Label(root, text="").pack()
 btnGo = Button(f2, text="Go", padx=6, pady=6, bd=2, fg="black", font=(
         'arial', 12, 'bold'), width=14, height=1, command=go).grid(row=0,
                                                                    column=0)
